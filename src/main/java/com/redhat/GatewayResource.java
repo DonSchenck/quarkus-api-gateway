3a50d9fbed5224b254ee3f93471cd208dc652ccb
unknown <dschenck@win.redhat.com>
2022-11-09 16:27:31 -0500

    On port 8080 now

--- src/main/java/com/redhat/GatewayResource.java
+++ src/main/java/com/redhat/GatewayResource.java
@@ -2,6 +2,8 @@ package com.redhat;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
@@ -9,17 +11,19 @@ import org.eclipse.microprofile.faulttolerance.Fallback;
 import org.eclipse.microprofile.faulttolerance.Timeout;
 
 @Path("/gateway")
+@Produces(MediaType.APPLICATION_JSON)
 public class GatewayResource {
     @RestClient
     MessageClient client;
 
     @GET
     @Timeout(1000)
-    @CircuitBreaker(
+    @CircuitBreaker
+    (
         requestVolumeThreshold = 4,
         failureRatio = 0.5,
-        delay = 500,
-        successThreshold = 2
+        delay = 5000,
+        successThreshold = 4
     )
     @Fallback(fallbackMethod = "movieRecommendationsFallbackMessage")
     public String message() {
